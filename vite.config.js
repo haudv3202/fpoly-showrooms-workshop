1c662604efece4e38222e058c7c108b9f4a33d0b
Nguyen huu phu <phub3@gmail.com>
2024-03-19 21:31:15 +0700

    commit - client view

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
+                "resources/sass/app.scss",
+                "resources/js/app.js",
+                "resources/js/scripts.min.js",
+                "resources/css/styles.min.css",
+                "resources/css/home-default.css",
             ],
             refresh: true,
         }),
